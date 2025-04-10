3096e0b9749ffbe873ad937337820c049ad640c6
petros-pavlosypsilantis <pet_yps@hotmail.com>
2025-04-10 20:45:58 +0300

    add doc loader and db functionality

---
+++ deepsearcher/online_query.py
@@ -0,0 +1,3 @@
+from typing import List, Tuple
+from deepsearcher.vector_db.base import RetrievalResult
+
---
+++ deepsearcher/rag_searcher.py
@@ -0,0 +1,2 @@
+from deepsearcher.agent import ChainOfRAG, DeepSearch, NaiveRAG
+from deepsearcher.agent.rag_router import RAGRouter
