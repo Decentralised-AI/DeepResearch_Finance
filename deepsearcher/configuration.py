f483d58b25984d0e99a2da9d521355b2d5afdbce
Petros Ypsilantis <pet_yps@hotmail.com>
2025-05-04 18:18:00 +0100

    develop the chain of rag agent

--- deepsearcher/configuration.py
+++ deepsearcher/configuration.py
@@ -1,3 +1,7 @@
+import os
+from typing import Literal
+import yaml
+
 from deepsearcher.llm.base import BaseLLM
 from deepsearcher.vector_db.base import BaseVectorDB
 from deepsearcher.embedding.base import BaseEmbedding
@@ -12,6 +16,28 @@ from deepsearcher.vector_db.milvus import Milvus
 from deepsearcher.agent.deep_search import DeepSearch
 from deepsearcher.agent.chain_of_rag import ChainOfRAG
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+DEFAULT_CONFIG_YAML_PATH = os.path.join(current_dir, "..", "config.yaml")
+print(DEFAULT_CONFIG_YAML_PATH)
+
+class Configuration:
+
+    def __init__(self, config_path: str = DEFAULT_CONFIG_YAML_PATH):
+        config_data = self.load_config_data_from_yaml(config_path)
+        self.provide_settings = config_data["provide_settings"]
+        self.query_settings = config_data["query_settings"]
+        self.load_settings = config_data["load_settings"]
+
+    def load_config_data_from_yaml(self, config_path: str):
+
+        with open(config_path, "r") as file:
+            return yaml.safe_load(file)
+
+
+
+config = Configuration()
+print(config.provide_settings)
+
 llm: BaseLLM = None
 embedding_model: BaseEmbedding = None
 file_loader: BaseLoader = None
@@ -20,21 +46,25 @@ default_searcher: RAGRouter = None
 naive_rag: NaiveRAG = None
 
 
-def init_config(config):
+def init_config(config: Configuration):
 
     global llm, embedding_model, file_loader, vector_db, default_searcher, naive_rag
-    llm = OpenAISearch(config["model"], )
-    embedding_model = OpenAIEmbedding(config)
-    file_loader = PDFLoader(config)
-    vector_db = Milvus(config)
+    llm_config = config.provide_settings["llm"]["config"]
+    embedding_config = config.provide_settings["embedding"]["config"]
+    vector_db_config = config.provide_settings["vector_db"]["config"]
+
+    llm = OpenAISearch(llm_config)
+    embedding_model = OpenAIEmbedding(embedding_config)
+    file_loader = PDFLoader()
+    vector_db = Milvus(vector_db_config)
     default_searcher = RAGRouter(
-        llm = llm,
+        llm=llm,
         rag_agents=[
             DeepSearch(
                 llm=llm,
                 embedding_model=embedding_model,
                 vector_db=vector_db,
-                max_iter=config['max_iter'],
+                max_iter=config.query_settings['max_iter'],
                 route_collection=True,
                 text_window_splitter=True
             ),
@@ -42,7 +72,7 @@ def init_config(config):
                 llm=llm,
                 embedding_model=embedding_model,
                 vector_db=vector_db,
-                max_iter=config["max_iter"],
+                max_iter=config.query_settings["max_iter"],
                 route_collection=True,
                 text_window_splitter=True
             )
