75857928972376091e5a365c26a6f755fdfb04e8
Petros Ypsilantis <pet_yps@hotmail.com>
2025-05-11 23:27:09 +0100

    develop the structured data db agent

--- deepsearcher/agent/structured_data_analyzer.py
+++ deepsearcher/agent/structured_data_analyzer.py
@@ -1,18 +1,33 @@
-
+from langchain_openai import ChatOpenAI
 from deepsearcher.agent.base import describe_class
+
+from typing import Dict, List
 
 @describe_class(
     "This can perform calculations on structured data that are available on excel files"
     "It is very suitable for performing analytics on companies fundamental such as: the 'price' of the share, the"
     " market cap, enterprice values, PEG Ratio, Price/Book, earnings, p/e ratios and etc."
 )
+
+
 class StructureDBAgent:
 
     def __init__(
             self,
             llm):
 
-        self.llm = llm
+        self.tools =
+        self.llm = ChatOpenAI(model="gpt-4o-mini")
+
+    def bind_tools(self, tools: List):
+        if tools:
+            self.tools = {t.name: t for t in tools}
+            self.llm = self.llm.bind_tools(tools)
+
+
+
+
+
 
 
 
